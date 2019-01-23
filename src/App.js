f891ed47886fa4b436b07e697287058e125b67a6
Gibran Khrisna Putra <gibrankhrisnaputra@gmail.com>
2019-01-24 01:20:28 +0700

    Add react-router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component, Fragment } from 'react'
+import { Route } from 'react-router-dom'
+import './App.css'
+
+import Home from './pages/Home'
+import UserList from './pages/UserList'
+import SignUp from './pages/SignUp'
+import SignIn from './pages/SignIn'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-
-      </div>
-    );
+      <Fragment>
+        <Route path="/" exact component={Home} />
+        <Route path="/users" exact component={UserList} />
+        <Route path="/signup" exact component={SignUp} />
+        <Route path="/signin" exact component={SignIn} />
+      </Fragment>
+    )
   }
 }
 
-export default App;
+export default App
