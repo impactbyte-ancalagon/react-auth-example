445a1b0622dd03d82ad9cc66ce7899c3a88d2b5b
Gibran Khrisna Putra <gibrankhrisnaputra@gmail.com>
2019-01-24 01:22:52 +0700

    Add protection for routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component, Fragment } from 'react'
-import { Route } from 'react-router-dom'
+import { Route, Redirect } from 'react-router-dom'
 import './App.css'
 
 import Home from './pages/Home'
@@ -8,13 +8,52 @@ import SignUp from './pages/SignUp'
 import SignIn from './pages/SignIn'
 
 class App extends Component {
+  state = {
+    isAuthenticated: false
+  }
+
+  updateAuthStatus = value => {
+    this.setState({ isAuthenticated: value })
+  }
+
   render() {
+    const { isAuthenticated } = this.state
+
     return (
       <Fragment>
         <Route path="/" exact component={Home} />
-        <Route path="/users" exact component={UserList} />
-        <Route path="/signup" exact component={SignUp} />
-        <Route path="/signin" exact component={SignIn} />
+        <Route
+          path="/users"
+          render={props =>
+            isAuthenticated ? (
+              <UserList {...props} />
+            ) : (
+              <Redirect
+                to={{ pathname: '/signin', state: { from: props.location } }}
+              />
+            )
+          }
+        />
+        <Route
+          path="/signup"
+          render={props =>
+            !isAuthenticated ? (
+              <SignUp {...props} />
+            ) : (
+              <Redirect to={{ pathname: '/users' }} />
+            )
+          }
+        />
+        <Route
+          path="/signin"
+          render={props =>
+            !isAuthenticated ? (
+              <SignIn {...props} updateAuthStatus={this.updateAuthStatus} />
+            ) : (
+              <Redirect to={{ pathname: '/users' }} />
+            )
+          }
+        />
       </Fragment>
     )
   }
