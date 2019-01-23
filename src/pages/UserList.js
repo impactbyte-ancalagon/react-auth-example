2270d7daa2da92a856a4e8557373b84d56edd670
Gibran Khrisna Putra <gibrankhrisnaputra@gmail.com>
2019-01-24 01:22:21 +0700

    Create users list page

---
+++ src/pages/UserList.js
@@ -0,0 +1,29 @@
+import React, { Component, Fragment } from 'react'
+import Cookies from 'js-cookie'
+import Axios from 'axios'
+
+export default class UserList extends Component {
+  state = {
+    users: []
+  }
+
+  async componentDidMount() {
+    const token = Cookies.get('token')
+
+    const {
+      data: { users }
+    } = await Axios.get('http://localhost:8000/api/users', {
+      headers: { Authorization: `Bearer ${token}` }
+    })
+
+    this.setState({ users })
+  }
+
+  render() {
+    const { users } = this.state
+
+    return (
+      <ul>{users && users.map((user, i) => <li key={i}>{user.name}</li>)}</ul>
+    )
+  }
+}
